149be8e0ff3c5f4fa4683a91bea6184d79efe2c6
Thinkpad <m15210580711@163.com>
2018-09-05 18:01:13 +0800

    'banner'

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption"><!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img :src="item.imgUrl" alt="" class="swiper-img">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/66/e5a5cec881702f02.jpg_750x200_67bb5691.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1808/8a/1957a0fb58ad0402.jpg_750x200_e72b8c0f.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff;
+  .wrapper
+    width:100%
+    height:0
+    overflow: hidden
+    padding-bottom: 27%
+    background: #fffffe
+    .swiper-img
+      width:100%
+</style>
