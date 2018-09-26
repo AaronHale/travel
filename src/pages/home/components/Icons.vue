e91b6edfd8ab2f47241e778084ef402926668b38
Thinkpad <m15210580711@163.com>
2018-09-26 15:29:50 +0800

    add recommend

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -53,40 +53,42 @@ export default {
 <style lang="stylus" scoped>
   @import "../../../assets/styles/varibles.styl"
   @import "../../../assets/styles/mixins.styl"
-  .icons
+  .icons >>> .swiper-container
     height:0
     padding-bottom:50%
     overflow: hidden
-  .icon
-    position relative
-    overflow hidden
-    width: 25%
-    height 0
-    float:left
-    padding-bottom 25%
-    .icon-img
+  .icons
+    margin-top: .1rem
+    .icon
+      position relative
+      overflow hidden
+      width: 25%
+      height 0
+      float:left
+      padding-bottom 25%
+      .icon-img
+        position absolute
+        top 0
+        left 0
+        right 0
+        bottom .44rem
+        box-sizing border-box
+        padding .1rem
+        width: 80%
+        margin: 0 auto
+        text-align: center
+        .icon-img-content
+          display block
+          margin 0 auto
+          height 100%
+    .icon-desc
       position absolute
-      top 0
       left 0
       right 0
-      bottom .44rem
-      box-sizing border-box
-      padding .1rem
-      width: 80%
-      margin: 0 auto
-      text-align: center
-      .icon-img-content
-        display block
-        margin 0 auto
-        height 100%
-  .icon-desc
-    position absolute
-    left 0
-    right 0
-    bottom 0
-    height .44rem
-    line-height .44rem
-    text-align center
-    color $darkTextColor
-    ellipsis()
+      bottom 0
+      height .44rem
+      line-height .44rem
+      text-align center
+      color $darkTextColor
+      ellipsis()
 </style>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,73 @@
+<template>
+  <div >
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl" alt="">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data() {
+    return {
+      recommendList: [
+        {id: '0001', imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_200x200_99ae30ee.jpg', title: '北京欢乐谷', desc: '远离城市喧嚣，感受自然之美'},
+        {id: '0002', imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_200x200_99ae30ee.jpg', title: '北京欢乐谷', desc: '远离城市喧嚣，感受自然之美'},
+        {id: '0003', imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_200x200_99ae30ee.jpg', title: '北京欢乐谷', desc: '远离城市喧嚣，感受自然之美'},
+        {id: '0004', imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_200x200_99ae30ee.jpg', title: '北京欢乐谷', desc: '远离城市喧嚣，感受自然之美'},
+        {id: '0005', imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_200x200_99ae30ee.jpg', title: '北京欢乐谷', desc: '远离城市喧嚣，感受自然之美'},
+        {id: '0006', imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_200x200_99ae30ee.jpg', title: '北京欢乐谷', desc: '远离城市喧嚣，感受自然之美'},
+        {id: '0007', imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_200x200_99ae30ee.jpg', title: '北京欢乐谷', desc: '远离城市喧嚣，感受自然之美'},
+        {id: '0008', imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_200x200_99ae30ee.jpg', title: '北京欢乐谷', desc: '远离城市喧嚣，感受自然之美'},
+        {id: '0009', imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_200x200_99ae30ee.jpg', title: '北京欢乐谷', desc: '远离城市喧嚣，感受自然之美'}
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "../../../assets/styles/mixins.styl"
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .3rem
+  .item
+    display: flex
+    height:1.9rem
+    overflow:hidden
+    .item-img
+      width:1.7rem;
+      height:1.7rem
+      padding:.1rem
+    .item-info
+        min-width: 0
+        flex:1
+        padding: .1rem
+        .item-title
+          line-height: .54rem
+          font-size: .32rem
+          ellipsis()
+        .item-info
+          line-height: .4rem
+          color: #cccccc
+        .item-button
+          background:#ff9300
+          margin-top: .16rem
+          padding:.1rem
+          color: #ffffff
+          line-height:.44rem
+          border-radius:.06rem
+          ellipsis()
+</style>
