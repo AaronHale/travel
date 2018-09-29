5d7b1fbf8f08a4efc83989ec5a54acc099d9510d
Thinkpad <m15210580711@163.com>
2018-09-29 18:59:12 +0800

    add gallary

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="container">
+      <div class="wrapper">
+        <swiper :options="swiperOptions"><!-- slides 父组件传递swiperOptions给swiper子组件 -->
+          <swiper-slide v-for="(item,index) of imags" :key="index">
+            <img class="gallary-img" alt="" :src="item">
+          </swiper-slide>
+          <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+      </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'CommonGallary',
+  props: {
+    imags: {
+      Array,
+      default () {
+        return [
+          'http://img1.qunarzz.com/sight/p0/1410/34/f540a04367d06820af4dfeaf0ad40257.water.jpg_r_800x800_479f9b55.jpg',
+          'http://img1.qunarzz.com/wugc/p180/201306/16/7f08e81624346b1693835fbb.jpg_r_800x800_5f03ad73.jpg'
+        ]
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOptions: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observeParents: true,
+        observer: true
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .container >>> .swiper-container
+    overflow inherit
+  .container
+    display flex
+    flex-direction column
+    justify-content center
+    z-index 99
+    position fixed
+    top 0
+    bottom 0
+    left 0
+    right 0
+    background #000
+    .wrapper
+      width 100%
+      padding-bottom 100%
+      height 0
+      .gallary-img
+        width 100%
+      .swiper-pagination
+        color #fff
+        bottom: -4rem
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg" alt="">
+      <div class="banner-info">
+        <p class="banner-title">大连圣亚海洋世界（AAAA景区）</p>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe626;</span>
+          39
+        </div>
+      </div>
+    </div>
+    <common-gallary :imags="imags" v-show="showGallary"></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from '../../../common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  components: {
+    CommonGallary
+  },
+  data () {
+    return {
+      showGallary: false,
+      imags: [
+        'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_r_800x800_6edd8174.jpg',
+        'http://img1.qunarzz.com/sight/p0/1410/9d/fe8109ab5df1c9c324e74284fa802e72.water.jpg_r_800x800_b9a5440a.jpg'
+      ]
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    .banner-img
+      width 100%
+    .banner-info
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      color #fff
+      background-image linear-gradient(top,rgba(0,0,0,0),rgba(0,0,0,.8))
+      .banner-title
+        flex 1
+        font-size .32rem
+        padding  0 .2rem
+      .banner-number
+        margin-top .14rem
+        padding 0 .4rem
+        line-height .32rem
+        height .32rem
+        border-radius .2rem
+        background rgba(0, 0, 0, .8)
+        font-size .24rem
+        .banner-icon
+          font-size .24rem
+</style>
